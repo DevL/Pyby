97c5e2da49fdb4a5867587efc3d5c0053a1f5355
Lennart Fridén <lennart@devl.se>
2022-03-08 15:02:46 +0100

    Broken attempt to determine arity of mapping function [ci skip]
    This doesn't work for builtin functions and making assumptions is the mother...

--- src/pyby/enumerable_dict.py
+++ src/pyby/enumerable_dict.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from inspect import signature
 from .enumerable import Enumerable
 from .enumerable_list import EnumerableList
 
@@ -13,6 +14,36 @@ class EnumerableDict(Enumerable, UserDict):
 
     def map(self, func=None):
         if func:
-            return EnumerableList(func(key, value) for key, value in self.each())
+            return EnumerableList(func(*kv) for kv in self.each())
+            # return EnumerableList(func(key, value) for key, value in self.each())
         else:
             return iter(self.items())
+
+
+def _safe_arity(func):
+    """
+    Makes a wild-ass guess that builtin functions without a signature have an arity of 1.
+
+    >>> _safe_arity(dict)
+    1
+    """
+    try:
+        return _arity(func)
+    except ValueError:
+        return 1
+
+
+def _arity(func):
+    """
+    >>> _arity("abc".upper)
+    0
+
+    >>> _arity(str.upper)
+    1
+
+    >>> _arity(lambda x, y: x + y)
+    2
+
+    Note that this cannot be used with certain builtin types such as dict.
+    """
+    return len(signature(func).parameters.keys())
