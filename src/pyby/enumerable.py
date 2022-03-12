e45c7f396510e1e2ad79f19dcc10f7f245d10ad1
Lennart Fridén <lennart@devl.se>
2022-03-12 11:23:21 +0100

    Make return type even more configurable WIP

--- src/pyby/enumerable.py
+++ src/pyby/enumerable.py
@@ -1,8 +1,18 @@
 from itertools import islice
+from functools import wraps
 from .object import RObject
 
 
 class Enumerable(RObject):
+    def as_enum(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            into = self._return_type()
+            result = func(self, *args, into=into, **kwargs)
+            return result
+
+        return wrapper
+
     def each(self, func=None):
         """
         The basis for all the functionality of any enumerable.
@@ -10,15 +20,15 @@ class Enumerable(RObject):
         """
         raise NotImplementedError("'each' must be implemented by a subclass")
 
-    def compact(self):
+    @as_enum
+    def compact(self, into=None):
         """
         Returns an enumerable of the elements with None values removed.
         """
-        return self._as_enumerable(
-            (item for item in self.each() if _to_tuple(item)[-1] is not None)
-        )
+        return into(item for item in self.each() if _to_tuple(item)[-1] is not None)
 
-    def first(self, number=None):
+    @as_enum
+    def first(self, number=None, into=None):
         """
         Returns the first element or a given number of elements.
         With no argument, returns the first element, or `None` if there is none.
@@ -27,9 +37,10 @@ class Enumerable(RObject):
         if number is None:
             return next(self.each(), None)
         else:
-            return self._as_enumerable(islice(self.each(), number))
+            return into(islice(self.each(), number))
 
-    def map(self, func=None):
+    @as_enum
+    def map(self, func=None, into=None):
         """
         Returns the result of mapping a function over the elements.
         The mapping function takes a single argument for sequences and two arguments for mappings.
@@ -37,14 +48,11 @@ class Enumerable(RObject):
         Also available as the alias `collect`.
         """
         if func:
-            return self._as_enumerable(func(*_to_tuple(item)) for item in self.each())
+            return into(func(*_to_tuple(item)) for item in self.each())
         else:
             return self.each()
 
     collect = map
-
-    def _as_enumerable(self, iterable):
-        return self._return_type()(iterable)
 
     def _return_type(self):
         """
