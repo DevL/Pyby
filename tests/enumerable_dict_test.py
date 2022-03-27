8c4b07dd5e2a41f5b94b975723d881bf2ac890b2
Lennart Fridén <lennart@devl.se>
2022-03-27 07:26:58 +0200

    Further refactor tests

--- tests/enumerable_dict_test.py
+++ tests/enumerable_dict_test.py
@@ -1,63 +1,65 @@
+import pytest
 from inspect import isgenerator
 from pyby import EnumerableDict, EnumerableList
 from .test_utils import Seen
 
 
-def test_each_with_a_function_calls_it_once_for_each_item():
+@pytest.fixture
+def enumerable_dict():
+    return EnumerableDict(a=1, b=2, c=3)
+
+
+@pytest.fixture
+def empty_dict():
+    return EnumerableDict()
+
+
+def test_each_with_a_function_calls_it_once_for_each_item(enumerable_dict):
     seen = Seen()
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
     enumerable_dict.each(seen)
     assert seen == [("a", 1), ("b", 2), ("c", 3)]
 
 
-def test_each_without_a_function_yields_each_item():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_each_without_a_function_yields_each_item(enumerable_dict):
     enumerator = enumerable_dict.each()
     assert isgenerator(enumerator)
     assert list(enumerator) == [("a", 1), ("b", 2), ("c", 3)]
 
 
-def test_map_with_a_function_calls_it_once_for_each_item_and_returns_an_enumerable_list():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_map_with_a_function_returns_an_enumerable_list(enumerable_dict):
     result = enumerable_dict.map(lambda key, value: (key.upper(), value + 1))
     assert isinstance(result, EnumerableList)
     assert result == [("A", 2), ("B", 3), ("C", 4)]
 
 
-def test_map_without_a_function_yields_each_item():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_map_without_a_function_yields_each_item(enumerable_dict):
     enumerator = enumerable_dict.map()
     assert isgenerator(enumerator)
     assert list(enumerator) == [("a", 1), ("b", 2), ("c", 3)]
 
 
-def test_first():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_first(enumerable_dict):
     assert enumerable_dict.first() == ("a", 1)
 
 
-def test_first_with_number_of_elements_specified():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_first_with_number_of_elements_specified(enumerable_dict):
     result = enumerable_dict.first(2)
     assert isinstance(result, EnumerableList)
     assert result == [("a", 1), ("b", 2)]
 
 
-def test_first_with_fewer_elements_than_asked_for():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_first_with_fewer_elements_than_asked_for(enumerable_dict):
     result = enumerable_dict.first(5)
     assert isinstance(result, EnumerableList)
     assert result == [("a", 1), ("b", 2), ("c", 3)]
 
 
-def test_first_when_empty():
-    enumerable_dict = EnumerableDict()
-    assert enumerable_dict.first() is None
+def test_first_when_empty(empty_dict):
+    assert empty_dict.first() is None
 
 
-def test_first_when_empty_when_asked_for_a_number_of_elements():
-    enumerable_dict = EnumerableDict()
-    result = enumerable_dict.first(5)
+def test_first_when_empty_when_asked_for_a_number_of_elements(empty_dict):
+    result = empty_dict.first(5)
     assert isinstance(result, EnumerableList)
     assert result == []
 
@@ -69,15 +71,13 @@ def test_compact():
     assert result == {"a": 1, "b": 2, "c": 3}
 
 
-def test_compact_when_empty():
-    enumerable_dict = EnumerableDict()
-    result = enumerable_dict.compact()
+def test_compact_when_empty(empty_dict):
+    result = empty_dict.compact()
     assert isinstance(result, EnumerableDict)
     assert result == {}
 
 
-def test_compact_when_not_containing_any_None_values():
-    enumerable_dict = EnumerableDict(a=1, b=2, c=3)
+def test_compact_when_not_containing_any_None_values(enumerable_dict):
     result = enumerable_dict.compact()
     assert isinstance(result, EnumerableDict)
     assert result == {"a": 1, "b": 2, "c": 3}
