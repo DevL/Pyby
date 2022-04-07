a48ced7fb7e0f52c9b3fbbabd6b32ed863cc0f4e
Lennart Fridén <lennart@devl.se>
2022-04-07 19:16:00 +0200

    Rearrange RObject
    This makes the respond_to function a direct alias of RObject.respond_to

--- src/pyby/object.py
+++ src/pyby/object.py
@@ -1,18 +1,21 @@
 class RObject:
+    """
+    A base class to enrich Python objects with additional functionality.
+    """
+
     def respond_to(self, name):
-        return respond_to(self, name)
+        """
+        Returns True if the object has a callable property with the given name.
+
+        Inspired by Ruby's respond_to method.
+
+        >>> respond_to(dict(), "get")
+        True
+
+        >>> respond_to(1, "get")
+        False
+        """
+        return callable(getattr(self, name, False))
 
 
-def respond_to(obj, name):
-    """
-    Returns True if the object has a callable property with the given name.
-
-    Inspired by Ruby's respond_to method.
-
-    >>> respond_to(dict(), "get")
-    True
-
-    >>> respond_to(1, "get")
-    False
-    """
-    return callable(getattr(obj, name, False))
+respond_to = RObject.respond_to
