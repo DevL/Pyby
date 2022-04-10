c321a40383b93fbfec7542d9c357cff546c62944
Lennart Fridén <lennart@devl.se>
2022-04-10 22:52:32 +0200

    Add doctests to the Seen test helper class

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -14,7 +14,20 @@ def pass_through(value, *values):
 
 class Seen(list):
     def __bool__(self):
+        """
+        >>> bool(Seen())
+        True
+        """
         return True
 
     def __call__(self, element):
+        """
+        >>> seen = Seen([1, 2])
+        >>> seen(3)
+        >>> seen
+        Seen([1, 2, 3])
+        """
         self.append(element)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({super().__repr__()})"
